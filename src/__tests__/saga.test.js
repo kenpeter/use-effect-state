67494f5b53ee90ce2d85caa5de62480a3561718a
Gary Liang <gary.liang@nab.com.au>
2019-12-04 11:49:56 +1100

    how to mock

---
+++ src/__tests__/saga.test.js
@@ -0,0 +1,38 @@
+// Follow this tutorial: https://medium.com/@lucaspenzeymoog/mocking-api-requests-with-jest-452ca2a8c7d7
+import SagaTester from 'redux-saga-tester';
+import mockAxios from 'axios';
+import reducer from '../reducer';
+import {getItemsSaga} from '../saga';
+
+const initialState = {
+  reducer: {
+    loading: true,
+    items: []
+  }
+};
+
+const options = {onError: console.error.bind(console)};
+
+describe('Saga', () => {
+  beforeEach(() => {
+    mockAxios.get.mockImplementationOnce(() => Promise.resolve({}));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('Showcases the tester API', async () => {
+    const sagaTester = new SagaTester({
+      initialState,
+      reducers: {reducer: reducer},
+      middlewares: [],
+      options
+    });
+
+    sagaTester.start(getItemsSaga);
+
+    sagaTester.dispatch({type: 'ITEMS_GET'});
+    await sagaTester.waitFor('ITEMS_GET_SUCCESS');
+  });
+});
--- src/saga.js
+++ src/saga.js
@@ -1,7 +1,8 @@
 import {all, call, put, takeEvery} from 'redux-saga/effects';
 
 function* getItems() {
-  const endpoint = 'https://jsonplaceholder.typicode.com/todos/1';
+  //const endpoint = 'https://jsonplaceholder.typicode.com/todos/1';
+  const endpoint = 'http://sdfds';
   const response = yield call(fetch, endpoint);
   const items = yield response.json();
   yield put({type: 'ITEMS_GET_SUCCESS', items: items});
